fc08ccf9e23c7d255d28d5d84fc6b55b7b438e53
victra <victra.sonata@gmail.com>
2017-04-13 16:00:14 +0700

    setup database

--- app/config/config.php
+++ app/config/config.php
@@ -11,8 +11,8 @@ return new \Phalcon\Config([
         'adapter'     => 'Mysql',
         'host'        => 'localhost',
         'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'password'    => 'root',
+        'dbname'      => 'phalcon-experiment_3_1_2',
         'charset'     => 'utf8',
     ],
     'application' => [
